e7deae314ccec775d5756aa2c43e5d8f5057101a
Tushar S Agrawal <tusharsagrawal16@gmail.com>
2021-09-20 17:39:54 +0530

    hello animation working

---
+++ scripts/hello.js
@@ -0,0 +1,22 @@
+var loading_timeline = anime.timeline();
+var updates = 0;
+loading_timeline.add({
+  targets: "#hello_svg >  path",
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: "easeInOutSine",
+  duration: 3000,
+  direction: "alternate",
+  loop: 0,
+});
+
+loading_timeline.add({
+  targets: "#hello_container",
+  opacity: "0",
+  easing: "easeInOutSine",
+  duration: 500,
+  direction: "normal",
+  loop: 0,
+  complete: function (anim) {
+    document.getElementById("hello_container").remove();
+  },
+});
