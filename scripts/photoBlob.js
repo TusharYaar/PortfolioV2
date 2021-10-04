34ceab9bace36459a2ea65ecf1bc0c4ebc70e96e
Tushar S Agrawal <tusharsagrawal16@gmail.com>
2021-10-04 15:46:57 +0530

    Contact Section Design for contact section made converted blob variables to array update timing for waves update margin in skills section

--- scripts/photoBlob.js
+++ scripts/photoBlob.js
@@ -1,7 +1,7 @@
 anime({
   targets: "#photo_blob > path",
   fill: "#FAA307",
-  d: [blob3, blob2, blob1, blob4],
+  d: blob,
   easing: "easeInOutQuad",
   duration: 8000,
   loop: true,
@@ -10,9 +10,18 @@ anime({
 
 anime({
   targets: "#skills_transition_wave > path",
-  d: [wave3, wave2, wave1, wave4, wave6, wave5],
+  d: wave,
   easing: "easeInOutQuad",
-  duration: 16000,
+  duration: 24000,
+  loop: true,
+  direction: "alternate",
+});
+
+anime({
+  targets: "#contact_transition_wave > path",
+  d: topWave,
+  easing: "easeInOutQuad",
+  duration: 24000,
   loop: true,
   direction: "alternate",
 });
