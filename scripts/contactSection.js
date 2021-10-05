5d58dd0d9364c2b1979ecc3104d10e89d9e1cb02
Tushar S Agrawal <tusharsagrawal16@gmail.com>
2021-10-05 13:25:18 +0530

    initialized sanity

--- scripts/contactSection.js
+++ scripts/contactSection.js
@@ -1,15 +1,20 @@
 document.getElementById("send_form").addEventListener("click", async (e) => {
   e.preventDefault();
-  // ! Add Validation
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value.toLowerCase();
+  let job = document.getElementById("job").value;
+  if (name.length < 0 || email.length < 0 || job.length < 0)
+    return alert("Empty field/s");
+  if (!email.match(EMAIL_REGEX)) return alert("Invalid email");
   try {
     const response = await fetch(
       "https://formsubmit.io/send/82dca637-f762-4592-88cb-5b17f5fcce4d",
       {
         method: "POST",
         data: {
-          name: document.getElementById("name").value,
-          email: document.getElementById("email").value,
-          job: document.getElementById("job").value,
+          name,
+          email,
+          job,
         },
       }
     );
