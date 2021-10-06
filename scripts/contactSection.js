935a1006dd03088ad5f316eeac38ec2ceb08d7a1
Tushar S Agrawal <tusharsagrawal16@gmail.com>
2021-10-06 20:12:45 +0530

    contact section done

--- scripts/contactSection.js
+++ scripts/contactSection.js
@@ -1,3 +1,30 @@
+/**
+ *
+ */
+
+const sending_timeline = anime.timeline({
+  autoplay: false,
+  complete: () => {
+    sendForm();
+    console.log("Sending timeline");
+    // anime.set("#send_form", { translateX: "-100vw" });
+  },
+});
+sending_timeline
+  .add({
+    targets: "#send_form",
+    translateX: -10,
+    duration: 500,
+    easing: "easeInOutQuad",
+  })
+  .add({
+    targets: "#send_form",
+    translateX: 100,
+    opacity: 0,
+    duration: 500,
+    easing: "easeInOutQuad",
+  });
+
 document.getElementById("send_form").addEventListener("click", async (e) => {
   e.preventDefault();
   let name = document.getElementById("name").value;
@@ -6,17 +33,36 @@ document.getElementById("send_form").addEventListener("click", async (e) => {
   if (name.length < 0 || email.length < 0 || job.length < 0)
     return alert("Empty field/s");
   if (!email.match(EMAIL_REGEX)) return alert("Invalid email");
+
+  document.getElementById("name").readOnly = true;
+  document.getElementById("email").readOnly = true;
+  document.getElementById("job").readOnly = true;
+  document.getElementById("send_form").disabled = true;
+  document.getElementById("send_form").innerHTML = "Sending...";
+  sending_timeline.play();
+});
+
+const sendForm = async () => {
   try {
-    let myForm = document.getElementById("contact_section_body");
-    let formData = new FormData(myForm);
-    console.log(formData);
+    const myForm = document.getElementById("contact_section_body");
+    const formData = new FormData(myForm);
     const response = await fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: new URLSearchParams(formData).toString(),
     });
-    console.log(response);
+    if (response.status === 200)
+      document.getElementById("send_form").innerHTML = "Sent Successfully.";
+    else document.getElementById("send_form").innerHTML = "Server Error :(";
   } catch (e) {
-    console.log(e);
+    document.getElementById("send_form").innerHTML = "Error :(";
+  } finally {
+    anime({
+      targets: "#send_form",
+      translateX: [-100, 0],
+      opacity: [0, 1],
+      duration: 500,
+      easing: "easeInOutQuad",
+    });
   }
-});
+};
