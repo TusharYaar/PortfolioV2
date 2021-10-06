e346b909ce187bc815635f418dd056d5de0a2414
Tushar S Agrawal <tusharsagrawal16@gmail.com>
2021-10-06 15:47:39 +0530

    Takeover function added Music playing and visualizer

--- scripts/playMusic.js
+++ scripts/playMusic.js
@@ -27,14 +27,76 @@ const seq = [
   { note: "E", delay: 200 },
   { note: "D", delay: 400 },
   { note: "C", delay: 400 },
-
-  // { note: "C8", delay: 1000 },
 ];
 
-let temp = anime.timeline({ autoplay: false });
+let music_wave = anime({
+  targets: "#music_wave> path",
+  d: musicWave,
+  easing: "easeInOutQuad",
+  duration: 2000,
+  loop: true,
+  autoplay: false,
+  direction: "alternate",
+});
+let music_timeline = anime.timeline({ autoplay: false });
+let takeover_timeline = anime.timeline({
+  autoplay: false,
+  complete: () => {
+    music_timeline.play();
+    music_wave.play();
+  },
+});
+
+takeover_timeline.add({
+  targets: "#takeover_container",
+  opacity: 1,
+  duration: 500,
+  easing: "easeInOutQuad",
+  translateY: [-50, 0],
+  begin: () => {
+    document.getElementById("takeover_container").style.display = "flex";
+  },
+  complete: () => {
+    document.getElementById("body").style.overflow = "hidden";
+    document.getElementById("music_wave").style.display = "block";
+    document.getElementById("music_wave").style.opacity = 1;
+  },
+});
+takeover_timeline.add({
+  targets: "#takeover_text",
+  opacity: [0, 1],
+  duration: 500,
+  easing: "easeInOutQuad",
+  translateY: [-50, 0],
+});
+takeover_timeline.add(
+  {
+    targets: ".takeover_countdown",
+    opacity: [0, 1],
+    duration: 500,
+    easing: "easeInOutQuad",
+    translateX: [-20, 0],
+    delay: function (el, i) {
+      return i * 1000;
+    },
+  },
+  "+=1000"
+);
+takeover_timeline.add({
+  targets: "#takeover_container",
+  opacity: 0,
+  translateY: [0, 50],
+  duration: 500,
+  easing: "easeInOutQuad",
+  complete: () => {
+    document.getElementById("takeover_container").style.display = "none";
+  },
+});
+
+document.getElementById("play_music").addEventListener("click", playMusic);
 
 seq.forEach(({ note, delay }) => {
-  temp.add(
+  music_timeline.add(
     {
       targets: `#note_${note}`,
       translateY: "-20",
@@ -54,6 +116,30 @@ seq.forEach(({ note, delay }) => {
   );
 });
 
-const playMusic = () => {
-  temp.res;
-};
+function playMusic() {
+  window.scroll({
+    top: 0,
+    behavior: "smooth",
+  });
+  takeover_timeline.play();
+}
+
+function endMusic() {
+  music_timeline.pause();
+  music_timeline.seek(0);
+  takeover_timeline.seek(0);
+  anime({
+    targets: "#music_wave",
+    opacity: 0,
+    translateY: -100,
+    easing: "easeInOutQuad",
+    duration: 500,
+    complete: () => {
+      document.getElementById("music_wave").removeAttribute("style");
+      document.querySelectorAll(".name_section_letter").forEach((el) => {
+        el.removeAttribute("style");
+      });
+      document.getElementById("body").style.overflow = "auto";
+    },
+  });
+}
